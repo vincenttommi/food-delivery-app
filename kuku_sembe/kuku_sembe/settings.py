e96c3e024c126cf435bc026b361f5234bed667f2
vincenttommi <vincenttommikorir@gmail.com>
2025-10-16 07:13:10 +0300

    setting up settings.py

--- kuku_sembe/kuku_sembe/settings.py
+++ kuku_sembe/kuku_sembe/settings.py
@@ -1,19 +1,28 @@
 from pathlib import Path
-
+from logging import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+from datetime import timedelta
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-adux5a)v)g%%97lzicwk^vboca2*mwatc8!yx2%*ai#4fhq3k9'
+
+SECRET_KEY = config(
+    "SECRET_KEY",default="django-insecure-adux5a)v)g%%97lzicwk^vboca2*mwatc8!yx2%*ai#4fhq3k9"
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+
+    "localhost",
+    "127.0.0.1",
+    "178.18.243.142",
+    "0.0.0.0"
+]
 
 
 # Application definition
@@ -25,7 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+    'drf_spectacular',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +83,60 @@ DATABASES = {
 }
 
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_EXPOSE_HEADERS = ['set-cookie'] #optional for debugging
+
+if DEBUG:
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+
+
+    SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_DOMAIN = None
+
+    ROOT_URLCONF = "kuku_sembe.urls"
+
+REST_FRAMEWORK =  {
+    "DEFAULT_SCHEMA_CLASS":"drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'user.authentication.CookieJWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS':False,
+    'BLACKLIST_AFTER_ROTATION':False, 
+    'AUTH_COOKIE':'access',  #custom key to identify your cookie
+    'AUTH_COOKIE_SECURE':True,
+    'AUTH_COOKIE_HTTP_ONLY':True,
+    'AUTH_COOKIE_PATH':'/',
+    'AUTH_COOKIE_SAMESITE':'None',
+
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -109,3 +177,49 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = config('EMAIL_BACKEND',default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool,default=True)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER',default='vincentttommikorir@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=EMAIL_HOST  EMAIL_HOST_USER)
+
+
+LOGGING = {
+    "version":1,
+    "disable_existing_loggers":False,
+    "handlers":{
+        "file":{
+            "level":"DEBUG",
+            "class":"logging.FileHandler",
+            "filename":"debug.log",
+        },
+    },
+    "loggers":{
+        "django":{
+            "handlers":["file"],
+            "level":"DEBUG",
+            "propagate":True
+        },
+    },
+
+}
+
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
